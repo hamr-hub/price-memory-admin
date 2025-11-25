bfe73574ad93f51829ba76a0ac96f2b61f85b765
heyongxian <heyongxian@kuaishou.com>
2025-11-25 16:52:40 +0800

    Initial commit from create-refine-app

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+
+import { useNotificationProvider } from "@refinedev/antd";
+import "@refinedev/antd/dist/reset.css";
+
+import dataProvider, { GraphQLClient } from "@refinedev/graphql";
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router";
+import { App as AntdApp } from "antd";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { ColorModeContextProvider } from "./contexts/color-mode";
+
+const API_URL = "https://your-graphql-url/graphql";
+
+const client = new GraphQLClient(API_URL);
+const gqlDataProvider = dataProvider(client);
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <AntdApp>
+            <DevtoolsProvider>
+              <Refine
+                dataProvider={gqlDataProvider}
+                notificationProvider={useNotificationProvider}
+                routerProvider={routerProvider}
+                options={{
+                  syncWithLocation: true,
+                  warnWhenUnsavedChanges: true,
+                  projectId: "68Tn3q-k8Oh2h-ZA1WwU",
+                }}
+              >
+                <Routes>
+                  <Route index element={<WelcomePage />} />
+                </Routes>
+                <RefineKbar />
+                <UnsavedChangesNotifier />
+                <DocumentTitleHandler />
+              </Refine>
+              <DevtoolsPanel />
+            </DevtoolsProvider>
+          </AntdApp>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
